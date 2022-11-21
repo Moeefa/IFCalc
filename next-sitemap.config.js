4864c2aaec38a5aab285c16fe2b7772ee84bb639
Moeefa <32604322+Moeefa@users.noreply.github.com>
2022-11-21 01:22:45 +0000

    Gera sitemap automaticamente após buildar.

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.NEXT_PUBLIC_URL || 'https://ifcalc.vercel.app',
+  generateRobotsTxt: false, // (optional)
+  // ...other options
+}
