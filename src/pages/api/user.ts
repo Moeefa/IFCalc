de180360df4000ab4ccf1a619022d45e0b7a6af5
Moeefa <moeefa@protonmail.com>
2023-04-11 08:31:30 -0400

    Update user.ts

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -38,10 +38,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
         suap: true,
         nome: b.disciplina.slice(b.disciplina.indexOf("-") + 1, b.disciplina.length).replace(/(III|II)/g, "").trim(), 
         notas: {
-          1: Number(b.nota_etapa_1.nota),
-          2: Number(b.nota_etapa_2.nota),
-          3: Number(b.nota_etapa_3.nota),
-          4: Number(b.nota_etapa_4.nota),
+          1: Number(b.nota_etapa_1.nota.replace(",", ".")),
+          2: Number(b.nota_etapa_2.nota.replace(",", ".")),
+          3: Number(b.nota_etapa_3.nota.replace(",", ".")),
+          4: Number(b.nota_etapa_4.nota.replace(",", ".")),
         },
       },
     ];
