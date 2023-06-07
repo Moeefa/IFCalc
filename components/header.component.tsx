eaa7da8e4bfb3e6b1e870e29852f8574fe630768
Moeefa <moeefa@protonmail.com>
2023-06-07 17:12:29 -0400

    Update header.component.tsx

--- components/header.component.tsx
+++ components/header.component.tsx
@@ -12,7 +12,7 @@ export default async function Header() {
     <>
       <h2>Teste2!</h2>
       
-      {!!session ? <h1>Teste3</h1> : <h1>Teste3</h1>}
+      {!!session ? <LogoutButton/> : <LoginButton/>}
 
       <header className="fixed w-full h-5 top-full bg-blue-200 backdrop-blur">
         <div className="float-right">
