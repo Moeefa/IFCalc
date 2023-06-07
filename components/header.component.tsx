d3c9d80a2859e2367ec1ba3a480ed6041b52e43d
Moeefa <moeefa@protonmail.com>
2023-06-07 18:27:41 -0400

    Update header.component.tsx

--- components/header.component.tsx
+++ components/header.component.tsx
@@ -11,7 +11,7 @@ export default async function Header() {
   return (
     <>      
       <header className="fixed w-full h-10 top-0 bg-blue-100 backdrop-blur">
-        <div className="flex justify-center">
+        <div className="flex items-center">
           <div className="float-right">
             {!!session ? <LogoutButton/> : <LoginButton/>}
           </div>
