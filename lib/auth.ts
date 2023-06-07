868b42ec7c7d31902761890ebdf181aa438d64ec
Moeefa <moeefa@protonmail.com>
2023-06-07 14:06:40 -0400

    Update auth.ts

--- lib/auth.ts
+++ lib/auth.ts
@@ -9,7 +9,7 @@ export const authOptions: NextAuthOptions = {
       return baseUrl
     },
     async session({ session, token, user }) {
-      if (session.user) session.user.id = token.sub;
+      session.user.id = token.sub;
       session.accessToken = token.accessToken;
       
       return session
