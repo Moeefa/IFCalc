5828d45f3e9766d781e4208184395aa466960eee
Moeefa <moeefa@protonmail.com>
2023-06-07 15:46:36 -0400

    Update header.component.tsx

--- components/header.component.tsx
+++ components/header.component.tsx
@@ -8,7 +8,7 @@ export default async function Header() {
   const session = await getServerSession(authOptions);
 
   return (
-    <header>
+    <header className="fixed w-full top-full bg-transparent backdrop-blur">
       <div className="float-right">
         {!!session ? <LogoutButton/> : <LoginButton/>}
       </div>
