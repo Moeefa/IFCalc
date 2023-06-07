1aa24669e75b0e5b22976b74842b8c331c3ff1dc
Moeefa <moeefa@protonmail.com>
2023-06-07 11:52:34 -0400

    Create auth.ts

---
+++ lib/auth.ts
@@ -0,0 +1,46 @@
+import type { NextAuthOptions } from 'next-auth';
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      return true
+    },
+    async redirect({ url, baseUrl }) {
+      return baseUrl
+    },
+    async session({ session, token, user }) {
+      session.user.id = token.sub;
+      session.accessToken = token.accessToken;
+      
+      return session
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      
+      return token;
+    },
+  },
+  providers: [
+    {
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      id: "suap",
+      name: "SUAP",
+      type: "oauth",
+      authorization: {
+        url: "https://suap.ifmt.edu.br/o/authorize",
+        params: { scope: "email identificacao" },
+      },
+      token: "https://suap.ifmt.edu.br/o/token/",
+      userinfo: "https://suap.ifmt.edu.br/api/eu/",
+      profile(profile) {
+        return {
+          id: profile.identificacao,
+          email: profile.email,
+        }
+      },
+    },
+  ],
+};
