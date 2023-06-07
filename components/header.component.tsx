942833fa531af8bcbe370a7693f1bc417db2e8ae
Moeefa <moeefa@protonmail.com>
2023-06-07 15:39:26 -0400

    Update header.component.tsx

--- components/header.component.tsx
+++ components/header.component.tsx
@@ -1,7 +1,16 @@
+import { authOptions } from '@/lib/auth';
+import { 
+  LoginButton,
+  LogoutButton,
+} from '@/components/buttons.component';
+
 export default async function Header() {
+  const session = await getServerSession(authOptions);
+
   return (
     <header>
       <div className="float-right">
+        {!!session ? <LogoutButton/> : <LoginButton/>}
       </div>
     </header>
   );
