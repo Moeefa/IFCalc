c4985d3bf68ed3f356ab5918c8edaf99cc1f3909
Moeefa <moeefa@protonmail.com>
2023-06-08 09:17:47 -0400

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,14 @@
 'use client';
 
-import {NextUIProvider} from '@nextui-org/react';
+import { NextUIProvider } from '@nextui-org/react';
+import { SessionProvider } from 'next-auth/react';
 
-export function Providers({children}: { children: React.ReactNode }) {
+export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <NextUIProvider>
-      {children}
-    </NextUIProvider>
+    <SessionProvider>
+      <NextUIProvider>
+        {children}
+      </NextUIProvider>
+    </SessionProvider>
   )
 }
