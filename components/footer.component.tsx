4f3c11d0c6d6c07d35c8f185e12868fcfdf037f7
Moeefa <moeefa@protonmail.com>
2023-06-08 12:01:18 -0400

    Update footer.component.tsx

--- components/footer.component.tsx
+++ components/footer.component.tsx
@@ -3,7 +3,7 @@ import { User, Link } from '@nextui-org/react';
 export default function Footer() {
   return (
     <>
-      <footer className="w-full h-16 absolute bottom-0 border-t border-default-200">
+      <footer className="w-full h-16 absolute bottom-0 border-t border-default-200 dark:border-default-100">
         <div className="flex w-full justify-center items-center">
           <User
             name="Luiz H. da Silva Xinaider"
