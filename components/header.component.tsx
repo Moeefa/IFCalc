d12c1899bce487cf0ef3a9c95ebbf5f4b6c50b8a
Moeefa <moeefa@protonmail.com>
2023-06-08 08:13:48 -0400

    Update header.component.tsx

--- components/header.component.tsx
+++ components/header.component.tsx
@@ -3,10 +3,6 @@
 import { authOptions } from '@/lib/auth';
 import { getServerSession } from 'next-auth';
 import { Navbar } from '@nextui-org/react';
-import { 
-  LoginButton,
-  LogoutButton,
-} from '@/components/buttons.component';
 
 export default async function Header({ children }: { children: React.ReactNode }) {
   const session = await getServerSession(authOptions);
