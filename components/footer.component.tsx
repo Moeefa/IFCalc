173a4c85d10ef379e240fcee5c903927307afb5f
Moeefa <moeefa@protonmail.com>
2023-06-08 13:59:42 -0400

    Update footer.component.tsx

--- components/footer.component.tsx
+++ components/footer.component.tsx
@@ -5,7 +5,7 @@ import { User, Link } from '@nextui-org/react';
 export default function Footer() {
   return (
     <>
-      <footer className="before:content-[''] dark:before:bg-background/50 before:bg-background/80 before:backdrop-saturate-150 before:backdrop-blur w-full h-16 absolute bottom-0 border-t border-default-200 dark:border-default-100">
+      <footer className="dark:bg-background/50 bg-background/80 backdrop-saturate-150 backdrop-blur w-full h-16 absolute bottom-0 border-t border-default-200 dark:border-default-100">
         <div className="flex h-full w-full justify-center items-center">
           <User
             name="Luiz H. da Silva Xinaider"
