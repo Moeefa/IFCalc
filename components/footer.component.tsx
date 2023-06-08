a15111da34298f58b6bd3982ed88f9c810cca775
Moeefa <moeefa@protonmail.com>
2023-06-08 11:39:59 -0400

    Create footer.component.tsx

---
+++ components/footer.component.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <>
+      <footer className="w-full h-6 absolute bottom-0">
+        <div className="flex w-full justify-center items-center">
+          <h1>Footer!<h1>
+        </div>
+      </footer>
+    </>
+  );
+}
