8618384a399c1a1cad0247d9ead08aaf476f9109
Moeefa <moeefa@protonmail.com>
2023-06-08 14:47:52 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
     <>
       <div className="flex flex-col justify-center">
         {[...Array(4)].map((_, i) => (
-          <Input className="basis-44" type="number" label={`${i+1}° bimestre`}/>
+          <Input className="basis-0.5 w-10" type="number" label={`${i+1}° bimestre`}/>
         ))}
       </div>
     </>
