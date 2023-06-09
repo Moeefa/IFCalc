fdcf16a68140954489357ec56fae5d8693ca2e8f
Moeefa <moeefa@protonmail.com>
2023-06-09 08:11:46 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
                 className="m-4 w-35" 
                 type="number" 
                 label={`${i+1}° bimestre`}
-                value={grade[`${i+1}`]}
+                value={grade[i]}
                 onValueChange={(value) => setGrade({ ...grade, [`${i+1}`]: Number(value) })}
               />
             ))}
