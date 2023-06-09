9263a34ff34e50af9bc79ea492b6f3d87b2712a5
Moeefa <moeefa@protonmail.com>
2023-06-08 22:14:33 -0400

    Update cards.component.tsx

--- components/cards.component.tsx
+++ components/cards.component.tsx
@@ -4,7 +4,7 @@ export function GradeYear({ grade = { 1: 0, 2: 0, 3: 0, 4: 0 } }: { grade?: { [i
   const final = ((((grade[1] || 0) * 2) + ((grade[2] || 0) * 2) + ((grade[3] || 0) * 3) +  ((grade[4] || 0) * 3)) / (2 + 2 + 3 + 3));
 
   return (
-    <Card className="max-w-[150px]">
+    <Card className="max-w-[250px]">
       <CardHeader className="flex gap-3">
         <div className="flex flex-col">
           <p className="text-md">{final >= 6 ? "Aprovado" : "Reprovado"}</p>
@@ -17,7 +17,7 @@ export function GradeYear({ grade = { 1: 0, 2: 0, 3: 0, 4: 0 } }: { grade?: { [i
       <Divider/>
       <CardFooter>
         Nota necessária no {grade[1] == 0 || grade[2] == 0 ? "1° ou 2°" : "3° ou 4°"} bimestre:
-        {Number((((6 - final) / (grade[1] == 0 || grade[2] == 0 ? 2 : 3)) * 10).toFixed(2)).toLocaleString("pt-BR")}
+        {' '}{Number((((6 - final) / (grade[1] == 0 || grade[2] == 0 ? 2 : 3)) * 10).toFixed(2)).toLocaleString("pt-BR")}
       </CardFooter>
     </Card>
   );
