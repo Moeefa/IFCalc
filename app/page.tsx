f33bc7082c0d55272c881dd9ae3c03a0ba6f0d63
Moeefa <moeefa@protonmail.com>
2023-06-09 07:28:55 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home() {
               />
             ))}
           </div>
-          <GradeCard grade={grade}/>
+          <GradeCard/>
         </div>
       </div>
     </>
