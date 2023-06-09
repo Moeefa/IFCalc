4cf065b2d63d615ec64936357336c3a50681534d
Moeefa <moeefa@protonmail.com>
2023-06-08 22:28:03 -0400

    Update cards.component.tsx

--- components/cards.component.tsx
+++ components/cards.component.tsx
@@ -4,19 +4,19 @@ export function GradeYear({ grade = { 1: 0, 2: 0, 3: 0, 4: 0 } }: { grade?: { [i
   const final = ((((grade[1] || 0) * 2) + ((grade[2] || 0) * 2) + ((grade[3] || 0) * 3) +  ((grade[4] || 0) * 3)) / (2 + 2 + 3 + 3));
 
   return (
-    <Card className="max-w-[280px] text-center">
+    <Card className="max-w-[280px]">
       <CardHeader className="flex gap-3">
         <div className="flex flex-col">
-          <p className="text-xl">{final >= 6 ? "Aprovado" : "Reprovado"}</p>
+          <p className="text-xl text-center">{final >= 6 ? "Aprovado" : "Reprovado"}</p>
         </div>
       </CardHeader>
       <Divider/>
       <CardBody>
-        <p>Nota final: {final}</p>
+        <p className="text-center">Nota final: {final}</p>
       </CardBody>
       <Divider/>
       <CardFooter>
-        <p className="text-xs">
+        <p className="text-xs text-center">
           Nota necessária no {grade[1] == 0 || grade[2] == 0 ? "1° ou 2°" : "3° ou 4°"} bimestre:
           {' '}{Number((((6 - final) / (grade[1] == 0 || grade[2] == 0 ? 2 : 3)) * 10).toFixed(2)).toLocaleString("pt-BR")}
         </p>
