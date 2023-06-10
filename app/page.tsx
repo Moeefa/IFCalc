523e5341877e1e9550b1511b2324755bdbc41b82
Moeefa <moeefa@protonmail.com>
2023-06-10 01:43:00 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,13 @@ import React, { useState } from 'react';
 import { Input } from '@nextui-org/react';
 import { FinalGrade as GradeCard } from '@/components/cards.component';
 
+function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+  let { value, min, max } = event.target;
+  value = Math.max(Number(min), Math.min(Number(max), Number(value)));
+
+  setGrade({ ...grade, [`${i+1}`]: event.target.value as unknown as number });
+};
+
 export default function Home() {
   const [grade, setGrade] = useState({ 
     "1": 0,
@@ -28,7 +35,7 @@ export default function Home() {
                 label={`${i+1}° bimestre`}
                 value={grade[(`${i+1}`) as keyof typeof grade] === 0 ? "" : grade[(`${i+1}`) as keyof typeof grade] as unknown as string}
                 placeholder="Insira sua nota"
-                onChange={(event) => setGrade({ ...grade, [`${i+1}`]: event.target.value as unknown as number })}
+                onChange={handleChange}
               />
             ))}
           </div>
