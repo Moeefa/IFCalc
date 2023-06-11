7e9c620844d6dda89e4765ca5acfe03679ae7a66
Moeefa <moeefa@protonmail.com>
2023-06-10 22:51:38 -0400

    Update cards.component.tsx

--- components/cards.component.tsx
+++ components/cards.component.tsx
@@ -13,19 +13,17 @@ export function FinalGrade({ grade = { "1": 0, "2": 0, "3": 0, "4": 0 } }: { gra
 
   return (
     <Card className="max-w-[280px]">
-      <CardHeader>
-        <div className="flex justify-center">
-          <p className="text-lg font-semibold text-default-600">{final >= 6 ? "Aprovado" : "Reprovado"}</p>
-        </div>
+      <CardHeader className="flex justify-center">
+        <p className={`text-lg font-semibold text-transparent bg-clip-text bg-gradient-to-b ${final >= 6 ? "from-blue-600 to-blue-800" : "from-red-600 to-red-800"}`}>{final >= 6 ? "Aprovado" : "Reprovado"}</p>
       </CardHeader>
       <CardBody>
-        <p className="text-center">Nota final: {Number(final.toFixed(2)).toLocaleString("pt-BR")}</p>
+        <p className="text-center text-default-400">Nota final: {Number(final.toFixed(2)).toLocaleString("pt-BR")}</p>
       </CardBody>
       {final >= 6 
         ? <></>
         : <>
             <CardFooter>
-              <p className="text-xs">
+              <p className="text-xs text-default-400">
                 Nota necessária no {grade["1"] == "0" || grade["2"] == "0" || grade[1] == "" || grade[2] == "" ? "1° ou 2°" : "3° ou 4°"} bimestre:
                 {' '}{Number((((6 - final) / (grade["1"] == "0" || grade["2"] == "0" || grade["1"] == "" || grade["2"] == "" ? 2 : 3)) * 10).toFixed(2)).toLocaleString("pt-BR")}
               </p>
