708c9a856270f193dbeb830c961656dc42b4dc37
Moeefa <moeefa@protonmail.com>
2023-06-10 23:09:14 -0400

    Update cards.component.tsx

--- components/cards.component.tsx
+++ components/cards.component.tsx
@@ -10,7 +10,20 @@ import {
 
 export function FinalGrade({ grade = { "1": 0, "2": 0, "3": 0, "4": 0 } }: { grade?: { [index: string]: number | string } }) {
   const final = (((Number(grade["1"] || 0) * 2) + (Number(grade["2"] || 0) * 2) + (Number(grade["3"] || 0) * 3) +  (Number(grade["4"] || 0) * 3)) / (2 + 2 + 3 + 3));
+  const exceeded = Number(grade["1"]) > 10 || Number(grade["2"]) > 10 || Number(grade["3"]) > 10 || Number(grade["4"]) > 10 ||
+                   Number(grade["1"]) < 0 || Number(grade["2"]) < 0 || Number(grade["3"]) < 0 || Number(grade["4"]) < 0;
 
+  if (exceeded) return (
+    <Card className="max-w-[280px]">
+      <CardHeader className="flex justify-center">
+        <p className="text-lg font-semibold text-transparent bg-clip-text bg-gradient-to-b from-red-600 to-red-800">Inválido</p>
+      </CardHeader>
+      <CardBody>
+        <p className="text-center text-default-400">A nota inserida é inválida!</p>
+      </CardBody>
+    </Card>
+  );
+  
   return (
     <Card className="max-w-[280px]">
       <CardHeader className="flex justify-center">
