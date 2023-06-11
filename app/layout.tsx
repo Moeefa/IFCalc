cfd40ec0d2cd07e35ff582d8acce0d2d20da79d7
Moeefa <moeefa@protonmail.com>
2023-06-11 17:00:49 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,6 @@ import Header from '@/components/header.component';
 import Footer from '@/components/footer.component';
 import { Providers } from './providers';
 import { Roboto } from 'next/font/google'
-import { authOptions } from '@/lib/auth';
-import { getServerSession } from 'next-auth';
 
 const roboto = Roboto({ subsets: ['latin'], weight: '400' })
 
@@ -13,13 +11,7 @@ export const metadata = {
   description: 'O IFCalc foi criado no intuito de auxiliar os alunos a visualizarem suas notas e aprovações. Os alunos podem salvar, editar e excluir suas notas no banco de dados.',
 }
 
-export default async function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  const session = await getServerSession(authOptions);
-
+export default async function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="pt-BR" className={`dark ${roboto.className}`}>
       <body>
