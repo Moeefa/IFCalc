17953d2f48873ea6d124776091c581c92c842099
Moeefa <moeefa@protonmail.com>
2023-06-11 18:43:07 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,21 @@
-import { authOptions } from '@/lib/auth';
-import { getServerSession } from 'next-auth';
+'use client';
+
+import { useSession } from 'next-auth/react';
 import { Tabs, Tab, Divider } from '@nextui-org/react';
 import { FinalTab } from '@/components/tabs.component';
 import SubjectCard from '@/components/subject.component';
-
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-  // const data = await 
+import useSWR from 'swr';
+ 
+export default function Home() {
+  const { data: session, status } = useSession();
+  const { data, error, isLoading } = useSWR(status === "authenticated" ? [`https://suap.ifmt.edu.br/api/v2/minhas-informacoes/boletim/${new Date().getFullYear()}/1/`, session.accessToken] : null, (url: string, token: string) => fetch(url, { headers: { Authorization: "Bearer " + token } }))
   
   return (
     <>
       <Tabs fullWidth variant="underlined" radius="full" aria-label="Médias">
         <Tab key="anual" title="Média anual">
           <FinalTab/>
-          {!!session &&
+          {!isLoading &&
             <>
               <Divider/>
               {session}
