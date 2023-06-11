2e3c74d2cd6f544f76e8efce8c907c4a12117c5f
Moeefa <moeefa@protonmail.com>
2023-06-10 22:18:13 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,33 +4,6 @@ import React, { useState } from 'react';
 import { Input } from '@nextui-org/react';
 import { FinalGrade as GradeCard } from '@/components/cards.component';
 
-function GradeInput({ i }: { i: number }) {
-  const [value, setValue] = React.useState("");
-
-  const validateGrade = (value) => value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
-
-  const validationState = React.useMemo(() => {
-    if (value === "") return undefined;
-
-    return validateEmail(value) ? "valid" : "invalid";
-  }, [value]);
-
-  return (
-    <Input 
-      id={`${i+1}_bim`}
-      size="sm" 
-      className="m-4 w-35" 
-      type="number"
-      max="10"
-      min="0"
-      label={`${i+1}° bimestre`}
-      value={grade[(`${i+1}`) as keyof typeof grade] as unknown as string}
-      placeholder="Insira sua nota"
-      onChange={(e) => handleChange(e, i)}
-    />
-  );
-}
-
 export default function Home() {
   const [grade, setGrade] = useState({ 
     "1": "",
@@ -40,10 +13,9 @@ export default function Home() {
   });
   
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>, i: number) => {
-    const { value, min, max } = event.target;
-    const newValue = Math.max(Number(0), Math.min(Number(10), Number(value)));
+    // const newValue = Math.max(0, Math.min(10, Number(event.target.value)));
 
-    setGrade({ ...grade, [`${i+1}`]: newValue });
+    setGrade({ ...grade, [`${i+1}`]: event.target.value });
   };
 
   return (
@@ -51,20 +23,28 @@ export default function Home() {
       <div className="flex justify-center">
         <div className="flex flex-col">
           <div className="lg:flex">
-            {[...Array(4)].map((_, i) => (
-              <Input 
-                id={`${i+1}_bim`}
-                size="sm" 
-                className="m-4 w-35" 
-                type="number"
-                max="10"
-                min="0"
-                label={`${i+1}° bimestre`}
-                value={grade[(`${i+1}`) as keyof typeof grade] as unknown as string}
-                placeholder="Insira sua nota"
-                onChange={(e) => handleChange(e, i)}
-              />
-            ))}
+            {[...Array(4)].map((_, i) => {
+              const value = grade[(`${i+1}`) as keyof typeof grade];
+              const state = Number(value) >= 0 && Number(value) <= 10 ? "valid" : "invalid";
+      
+              return (
+                <Input 
+                  id={`${i+1}_bim`}
+                  size="sm" 
+                  className="m-4 w-35" 
+                  type="number"
+                  max="10"
+                  min="0"
+                  label={`${i+1}° bimestre`}
+                  color={state === "valid" ? "default" : "warning"}
+                  validationState={state}
+                  errorMessage={state === "invalid" && "Insira um número entre 0 e 10"}
+                  value={value as unknown as string}
+                  placeholder="Insira sua nota"
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleChange(e, i)}
+                />
+              );
+            })}
           </div>
           <GradeCard grade={grade}/>
         </div>
