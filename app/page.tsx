8471ea8978e9f4b5fd4ed3d1c3ad8fe0576a6440
Moeefa <moeefa@protonmail.com>
2023-06-11 20:29:23 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,23 @@ import { useSession } from 'next-auth/react';
 import { Tabs, Tab, Divider } from '@nextui-org/react';
 import { FinalTab } from '@/components/tabs.component';
 import SubjectCard from '@/components/subject.component';
+import axios from 'axios';
 import useSWR from 'swr';
  
 export default function Home() {
-  const fetcher = (url: string, token: string) => fetch(url, { 
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + token,
-      }
-    })
-    .then(r => r.json());
+  const fetcher = (url, token) =>
+    axios
+      .get(url, { headers: { Authorization: "Bearer " + token } })
+      .then((res) => res.data);
   
   const { data: session, status } = useSession();
-  const { data, error, isLoading } = useSWR(status === "authenticated" ? [`https://suap.ifmt.edu.br/api/v2/minhas-informacoes/boletim/${new Date().getFullYear()}/1/`, session.accessToken] : null, ([url, token]) => fetcher(url, token));
-  
+  const { data, error, isLoading } = useSWR(
+    status === "authenticated" 
+      ? [`https://suap.ifmt.edu.br/api/v2/minhas-informacoes/boletim/${new Date().getFullYear()}/1/`, session.accessToken] 
+      : null, 
+    fetcher
+  );
+
   return (
     <>
       <Tabs fullWidth variant="underlined" radius="full" aria-label="Médias">
