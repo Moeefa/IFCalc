00af42ec9597a5a82be82021796909845dfb2ed3
Moeefa <moeefa@protonmail.com>
2023-06-26 14:12:50 -0400

    Mudança na posição do atributo do Navbar

--- components/header.component.tsx
+++ components/header.component.tsx
@@ -2,20 +2,20 @@
 
 import { useSession } from 'next-auth/react';
 import { Navbar, NavbarContent, NavbarItem, Skeleton } from '@nextui-org/react';
-import { 
+import {
   LoginButton,
   LogoutButton,
 } from '@/components/buttons.component';
 
 export default function Header() {
   const { data: session, status } = useSession();
-  
+
   return (
     <>
-      <Navbar position="fixed">
+      <Navbar position="sticky">
         <NavbarContent justify="end">
           <NavbarItem>
-            {status === "authenticated" ? <LogoutButton/> : <Skeleton isLoaded={status !== "loading"} className="rounded-full"><LoginButton/></Skeleton>}
+            {status === "authenticated" ? <LogoutButton /> : <Skeleton isLoaded={status !== "loading"} className="rounded-full"><LoginButton /></Skeleton>}
           </NavbarItem>
         </NavbarContent>
       </Navbar>
