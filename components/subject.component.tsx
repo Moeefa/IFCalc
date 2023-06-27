f652a17e0f172a155e7b15a282d120d64a783667
Moeefa <moeefa@protonmail.com>
2023-06-27 14:22:14 -0400

    Teste

--- components/subject.component.tsx
+++ components/subject.component.tsx
@@ -20,19 +20,19 @@ export default function Subject({ data }: { data: any }) {
               return (
                 <AccordionItem
                   key={i}
-                  startContent={Number(final.toFixed(1)).toLocaleString("pt-BR")}
+                  startContent={final.toFixed(2)}
                   title={e.nome}
                   aria-label={e.nome}
                   subtitle={<p className={`font-semibold text-transparent bg-clip-text bg-gradient-to-b ${final >= 6 ? "from-blue-600 to-blue-800" : "from-red-600 to-red-800"}`}>{final >= 6 ? "Aprovado" : "Reprovado"}</p>}
                 >
-                  <Divider className="mb-2" />
+                  {/*}<Divider className="mb-2" />
                   <div className="flex justify-center">
                     <div className="grid grid-cols-2 grid-rows-2 gap-2">
                       {[...Array(4)].map((_, i) => (
                         <p className="text-default-400">{i + 1}° BIMESTRE: <span className="text-black dark:text-white">{e.notas[`${i + 1}`]}</span></p>
                       ))}
                     </div>
-                  </div>
+                  </div>*/}
                 </AccordionItem>
               )
             })}
