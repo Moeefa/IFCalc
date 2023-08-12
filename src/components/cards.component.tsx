52f968637f24c21c925bc4ca1d9c5691955b47d1
Moeefa <moeefa@protonmail.com>
2023-08-12 07:10:34 -0400

    Mostra a nota e quanto falta sem arredondamentos, mais precisamente (o status de aprovado funciona normalmente)

--- src/components/cards.component.tsx
+++ src/components/cards.component.tsx
@@ -6,6 +6,11 @@ import {
   Divider,
 } from "@nextui-org/react";
 
+const toFixed = (n: number, precision: number) => {
+  const s = `${n}`, i = `${n}`.split('.'), p = Math.max(precision | 0, 0);
+  return Number(i[0] + (p ? `.${(i[1] ?? '').padEnd(p, '0')}`.slice(0, p + 1) : ''));
+};
+
 export function FinalCard({ grade = { "1": 0, "2": 0, "3": 0, "4": 0 } }: { grade?: { [index: string]: number | string } }) {
   const final = (((Number(grade["1"]) * 2) + (Number(grade["2"]) * 2) + (Number(grade["3"]) * 3) + (Number(grade["4"]) * 3)) / (2 + 2 + 3 + 3));
   const hasExceeded = Number(grade["1"]) > 10 || Number(grade["2"]) > 10 || Number(grade["3"]) > 10 || Number(grade["4"]) > 10 || Number(grade["1"]) < 0 || Number(grade["2"]) < 0 || Number(grade["3"]) < 0 || Number(grade["4"]) < 0;
@@ -17,7 +22,7 @@ export function FinalCard({ grade = { "1": 0, "2": 0, "3": 0, "4": 0 } }: { grad
         <p className={`text-lg font-semibold text-transparent bg-clip-text bg-gradient-to-b ${final >= 6 && !hasExceeded ? "from-blue-600 to-blue-800" : "from-red-600 to-red-800"}`}>{hasExceeded ? "Inválido" : final >= 6 ? "Aprovado" : "Reprovado"}</p>
       </CardHeader>
       <CardBody className="pt-0">
-        <p className="text-center">{hasExceeded ? "Nota inválida" : `Nota final: ${Number(final.toFixed(2)).toLocaleString("pt-BR")}`}</p>
+        <p className="text-center">{hasExceeded ? "Nota inválida" : `Nota final: ${toFixed(Number(final), 1).toLocaleString("pt-BR")}`}</p>
       </CardBody>
       {final >= 6 || hasExceeded
         ? <></>
@@ -26,7 +31,7 @@ export function FinalCard({ grade = { "1": 0, "2": 0, "3": 0, "4": 0 } }: { grad
           <CardFooter className="flex justify-center">
             <p className="text-xs text-center text-default-400">
               Nota necessária no {isEmpty ? "1° ou 2°" : "3° ou 4°"} bimestre:
-              {' '}{Number((((6 - final) / (isEmpty ? 2 : 3)) * 10).toFixed(1)).toLocaleString("pt-BR")}
+              {' '}{Number(((6 - final) / (isEmpty ? 2 : 3)) * 10).toLocaleString("pt-BR")}
             </p>
           </CardFooter>
         </>}
@@ -37,14 +42,14 @@ export function FinalCard({ grade = { "1": 0, "2": 0, "3": 0, "4": 0 } }: { grad
 export function BimestralCard({ grade = { notas: [''], conceito: '' } }: { grade?: { notas: string[]; conceito: string; } }) {
   const final = ((grade.notas.reduce((sum, a) => Number(sum) + Number(a), 0) / grade.notas.length) * 0.8) + Number(grade.conceito);
   const hasExceeded = grade.notas.some(e => Number(e) < 0 || Number(e) > 10);
-  
+
   return (
     <Card shadow="none" className="sm:w-72 w-11/12">
       <CardHeader className="flex justify-center">
         <p className={`text-lg font-semibold text-transparent bg-clip-text bg-gradient-to-b ${final >= 6 && !hasExceeded ? "from-blue-600 to-blue-800" : "from-red-600 to-red-800"}`}>{hasExceeded ? "Inválido" : final >= 6 ? "Aprovado" : "Reprovado"}</p>
       </CardHeader>
       <CardBody className="pt-0">
-        <p className="text-center">{hasExceeded ? "Nota inválida" : `Nota final: ${Number(final.toFixed(2)).toLocaleString("pt-BR")}`}</p>
+        <p className="text-center">{hasExceeded ? "Nota inválida" : `Nota final: ${toFixed(Number(final), 1).toLocaleString("pt-BR")}`}</p>
       </CardBody>
       {/*final >= 6 || hasExceeded
         ? <></>
