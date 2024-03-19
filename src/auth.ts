bd2f1fb770508824e5bbf22dc92becd6e469a8c8
Moeefa <moeefa@protonmail.com>
2024-03-19 13:57:45 -0400

    🐛 fix: suap oauth

--- src/auth.ts
+++ src/auth.ts
@@ -10,7 +10,6 @@ import type { JWT } from "next-auth/jwt";
 import NextAuth from "next-auth";
 
 export const config = {
-  basePath: "/auth",
   trustHost: true,
   secret: process.env.AUTH_SECRET,
   providers: [
@@ -20,11 +19,17 @@ export const config = {
       id: "suap",
       name: "SUAP",
       type: "oauth",
-      token: "https://suap.ifmt.edu.br/o/token/",
-      userinfo: "https://suap.ifmt.edu.br/api/eu/",
       authorization: {
         url: "https://suap.ifmt.edu.br/o/authorize",
-        params: { scope: "identificacao" },
+        params: { scope: "email identificacao" },
+      },
+      token: "https://suap.ifmt.edu.br/o/token/",
+      userinfo: "https://suap.ifmt.edu.br/api/eu/",
+      profile(profile) {
+        return {
+          id: profile.identificacao,
+          ...profile,
+        };
       },
     },
   ],
@@ -32,6 +37,29 @@ export const config = {
     strategy: "jwt",
     maxAge: 4 * 60 * 60,
     updateAge: 2 * 60 * 60,
+  },
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user && token) {
+        session.user.id = token.sub as string;
+        session.user.name = token.uid?.nome_social || token.uid?.nome_usual;
+        session.user.image = token.uid?.foto;
+        session.access_token = token.access_token;
+      }
+
+      return session;
+    },
+    async jwt({ token, user, account }) {
+      token.access_token ??= account?.access_token;
+      token.uid ??= {
+        id: user?.id,
+        identificacao: user?.identificacao,
+        nome_social: user?.nome_social,
+        nome_usual: user?.nome_usual,
+      };
+
+      return token;
+    },
   },
 } satisfies NextAuthConfig;
 
